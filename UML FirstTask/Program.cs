829f02c4676c767a5208c46b9221e8b70a2afde0
user <huseynovbabek1@gmail.com>
2023-11-22 21:19:00 +0400

    Add project files.

---
+++ UML FirstTask/Program.cs
@@ -0,0 +1,40 @@
+﻿#region FactoryMethod
+
+
+class Logistik
+{
+    private ITransport transport;
+
+    public void deliver()
+    {
+        transport.deliver();
+    }
+
+
+}
+
+
+interface ITransport
+{
+   public void deliver();
+}
+
+class Truck : ITransport
+{
+    public void deliver()
+    {
+        Console.WriteLine("truck");
+    }
+}
+
+class Airplane : ITransport
+{
+    public void deliver()
+    {
+        Console.WriteLine("airplane");
+    }
+}
+
+#endregion
+
+
